9377a10dea0e837aa3aad09c50ccc34cb1cb4232
Guido Boetsch <gboetsch93@gmail.com>
2026-08-21 18:23:26 -0300

    Updates

--- assets/CatalogPage-Cggf8ZlJ.js
+++ assets/CatalogPage-Cggf8ZlJ.js
@@ -1,4 +1,4 @@
-import{c as R,j as e,T as q,X as $,u as I,a as B,O as H,H as G,b as Q,d as V,S as U}from"./index-Djdn0I-X.js";import{L as F,f as _,b as h}from"./react-BdHa0J-c.js";import{f as X}from"./format-x7npCmSH.js";import"./firebase-CcXQt6GP.js";/**
+import{c as R,j as e,T as q,X as $,u as I,a as B,O as H,H as G,b as Q,d as V,S as U}from"./index-DSS-V9DR.js";import{L as F,f as _,b as h}from"./react-BdHa0J-c.js";import{f as X}from"./format-x7npCmSH.js";import"./firebase-lcB5PQZi.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
